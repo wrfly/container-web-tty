311c4eb9595eceb45c674ef4049e3b272177b54d
wrfly <mr.wrfly@gmail.com>
2018-11-25 23:09:38 +0800

    fix: grpc duplicate containers

--- run.go
+++ run.go
@@ -16,11 +16,16 @@ import (
 
 func run(c *cli.Context, conf config.Config) {
 	srvOptions := conf.Server
+
 	if len(conf.Backend.GRPC.Servers) > 0 {
 		srvOptions.ShowLocation = true
 	}
 	if err := utils.ApplyDefaultValues(srvOptions); err != nil {
 		logrus.Fatal(err)
+	}
+
+	if srvOptions.GrpcPort <= 0 && srvOptions.Port <= 0 {
+		logrus.Fatal("bad config, no port listenning")
 	}
 
 	containerCli, err := container.NewCliBackend(conf.Backend)
@@ -29,30 +34,32 @@ func run(c *cli.Context, conf config.Config) {
 	}
 	defer containerCli.Close()
 
-	srv, err := route.New(containerCli, srvOptions)
-	if err != nil {
-		logrus.Fatalf("Create server error: %s", err)
-	}
-
 	ctx, cancel := context.WithCancel(context.Background())
 	gCtx, gCancel := context.WithCancel(context.Background())
-
 	errs := make(chan error, 2)
-	go func() {
-		errs <- srv.Run(ctx, route.WithGracefullContext(gCtx))
-	}()
 
-	// run grpc server if port > 0
-	if conf.Server.GrpcPort > 0 {
-		grpcServer := proxy.New(conf.Backend.GRPC.Auth,
-			conf.Server.GrpcPort, containerCli)
+	// run HTTP server if port > 0
+	if srvOptions.Port > 0 {
 		go func() {
+			srv, err := route.New(containerCli, srvOptions)
+			if err != nil {
+				logrus.Fatalf("Create server error: %s", err)
+			}
+			errs <- srv.Run(ctx, route.WithGracefullContext(gCtx))
+		}()
+	}
+
+	// run grpc server if grpc-port > 0
+	if srvOptions.GrpcPort > 0 {
+		go func() {
+			grpcServer := proxy.New(conf.Backend.GRPC.Auth,
+				srvOptions.GrpcPort, containerCli)
 			errs <- grpcServer.Run(ctx)
 		}()
 	}
 
 	err = util.WaitSignals(errs, cancel, gCancel)
 	if err != nil && err != context.Canceled {
-		logrus.Fatalf("Server exist with error: %s", err)
+		logrus.Fatalf("Server exit with error: %s", err)
 	}
 }
